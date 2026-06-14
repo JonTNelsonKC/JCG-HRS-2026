ba58c155cb6027df63e4929a39d223f16b6e6bdd
JoelG8060 <joelgray8060@gmail.com>
2026-06-14 14:49:05 -0500

    Add files via upload

---
+++ functions/api/news.js
@@ -0,0 +1,21 @@
+import { getNewsDashboard } from "../_lib/nascar-data.js";
+
+const JSON_HEADERS = {
+  "content-type": "application/json; charset=utf-8",
+  "cache-control": "no-store"
+};
+
+function json(payload, status = 200) {
+  return new Response(JSON.stringify(payload), {
+    status,
+    headers: JSON_HEADERS
+  });
+}
+
+export async function onRequestGet() {
+  try {
+    return json(await getNewsDashboard());
+  } catch (error) {
+    return json({ ok: false, error: error.message }, 500);
+  }
+}
---
+++ src/nascar-data.js
@@ -0,0 +1,7 @@
+export {
+  buildDashboard,
+  buildMelonPatchDashboard,
+  getNewsDashboard,
+  getScheduleDashboard,
+  getWreckhouseDashboard
+} from "../functions/_lib/nascar-data.js";
